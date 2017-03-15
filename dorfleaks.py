a568ec8eb30b42fc70567125ed06ade25a7e22ff
Niklas Sombert <niklas@ytvwld.de>
2017-03-15 19:45:08 +0100

    Save who posted a tweet.

--- dorfleaks.py
+++ dorfleaks.py
@@ -2,7 +2,7 @@
 APP_KEY = ""
 APP_SECRET = ""
 
-from twython import Twython
+import twython
 import dateutil.parser
 import json
 from os import environ
@@ -26,7 +26,27 @@ if not saved:
 	saved["count"] = dict()
 	saved["ids"] = list()
 
-twitter = Twython(APP_KEY, APP_SECRET)
+twitter = twython.Twython(APP_KEY, APP_SECRET)
+
+if not "users" in saved:
+	print("Gathering additional data about existing tweets...")
+	users = dict()
+	for id in saved["ids"]:
+		try:
+			status = twitter.get(endpoint="statuses/show", params={"id": id})
+			user = status["user"]["screen_name"]
+		except twython.exceptions.TwythonError as exc:
+			if exc.error_code == 403:
+				print("The user who posted {} is no longer publicly visible.".format(id))
+				user = "n/a"
+			else:
+				raise
+		if not user in users:
+			users[user] = 1
+		else:
+			users[user] += 1
+		print("Saved {} by {}.".format(id, user))
+	saved["users"] = users # don't do this partially
 
 dorfleaks = twitter.search(q='#dorfleaks', count=1024, result_type="recent")["statuses"]
 
@@ -35,12 +55,17 @@ for status in dorfleaks:
 	if not status["retweeted"] and not status["text"].startswith("RT @"):
 		date = str(dateutil.parser.parse(status["created_at"]).date())
 		id = status["id"]
+		user = status["user"]["screen_name"]
 		if id not in saved["ids"]:
 			if not date in saved["count"]:
 				saved["count"][date] = 1
 			else:
 				saved["count"][date] += 1
-			print("Saved {}.".format(id))
+			if not user in saved["users"]:
+				saved["users"][user] = 1
+			else:
+				saved["users"][user] += 1
+			print("Saved {} on {} by {}.".format(id, date, user))
 			saved["ids"].append(id)
 		#print(status["text"], dateutil.parser.parse(status["created_at"]))
 
