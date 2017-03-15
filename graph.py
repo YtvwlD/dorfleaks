b749cc9a16d3d4cdcc9d746736cf8377af3e58bc
Niklas Sombert <niklas@ytvwld.de>
2017-03-15 16:14:03 +0100

    graph.py: Support plotting per_weekday_week.

--- graph.py
+++ graph.py
@@ -4,7 +4,7 @@ import argparse
 import sys
 
 parser = argparse.ArgumentParser(description="Plots various statistics about #dorfleaks")
-parser.add_argument("--what", type=str, choices=["per_date", "per_weekday"], default='per_date')
+parser.add_argument("--what", type=str, choices=["per_date", "per_weekday", "per_weekday_week"], default='per_date')
 parser.add_argument("--from_url", type=str, help="path to dorfleaks-saved.json", default="https://pub.ytvwld.de/dorfleaks.json", metavar="URL")
 parser.add_argument("--from_file", type=str, help="path to dorfleaks-saved.json", default="dorfleaks-saved.json", metavar="FILE")
 parser.add_argument("--to_file", type=str, help="output filename", default="plot.html", metavar="FILE")
@@ -45,6 +45,28 @@ elif args.what == "per_weekday":
         y = list(count.values())
     )])
     layout = plotly.graph_objs.Layout(title="tweets containing #dorfleaks per weekday")
+elif args.what == "per_weekday_week":
+    import datetime
+    count = {}
+    for date_iso in saved["count"].keys():
+        date = datetime.date(*map(int, date_iso.split("-"))) # Why is there no fromiso?
+        week = date.isocalendar()[1]
+        if not week in count.keys():
+            count[week] = {}
+            for day in range(0, 7):
+                count[week][day] = 0
+        count[week][date.weekday()] += saved["count"][date_iso]
+    data = []
+    for week in count.keys():
+        data += [plotly.graph_objs.Bar(
+            x = list(["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]),
+            y = list(count[week].values()),
+            name = "week #{}".format(week)
+        )]
+    layout = plotly.graph_objs.Layout(
+        title="tweets containung #dorfleaks per weekday in each week",
+        barmode="stack"
+    )
 else:
     raise Exception
 
