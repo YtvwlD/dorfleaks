0180b5e4fe7b1b65071a954910e72b8a4b6b2b05
Niklas Sombert <niklas@ytvwld.de>
2017-04-18 18:01:58 +0200

    Allow to save retroactively.

--- save.py
+++ save.py
@@ -6,6 +6,7 @@ import twython
 import dateutil.parser
 import json
 from os import environ
+from sys import argv
 
 if APP_KEY or APP_SECRET:
 	print("[WARN]: Please set the api secrets via environment.")
@@ -28,30 +29,7 @@ if not saved:
 
 twitter = twython.Twython(APP_KEY, APP_SECRET)
 
-if not "users" in saved:
-	print("Gathering additional data about existing tweets...")
-	users = dict()
-	for id in saved["ids"]:
-		try:
-			status = twitter.show_status(id=id)
-			user = status["user"]["screen_name"]
-		except twython.exceptions.TwythonError as exc:
-			if exc.error_code == 403:
-				print("The user who posted {} is no longer publicly visible.".format(id))
-				user = "n/a"
-			else:
-				raise
-		if not user in users:
-			users[user] = 1
-		else:
-			users[user] += 1
-		print("Saved {} by {}.".format(id, user))
-	saved["users"] = users # don't do this partially
-
-dorfleaks = twitter.search(q='#dorfleaks', count=1024, result_type="recent")["statuses"]
-
-for status in dorfleaks:
-	#print(status.keys())
+def save(saved, status):
 	if not status["retweeted"] and not status["text"].startswith("RT @"):
 		date = str(dateutil.parser.parse(status["created_at"]).date())
 		id = status["id"]
@@ -67,6 +45,42 @@ for status in dorfleaks:
 				saved["users"][user] += 1
 			print("Saved {} on {} by {}.".format(id, date, user))
 			saved["ids"].append(id)
+
+if len(argv) == 2:
+	try:
+		id = int(argv[-1])
+	except ValueError:
+		id = int(argv[-1].split("/")[-1])
+	print("Saving the specified tweet: {}".format(id))
+	status = twitter.show_status(id=id)
+	save(saved, status)
+
+else:
+	if not "users" in saved:
+		print("Gathering additional data about existing tweets...")
+		users = dict()
+		for id in saved["ids"]:
+			try:
+				status = twitter.show_status(id=id)
+				user = status["user"]["screen_name"]
+			except twython.exceptions.TwythonError as exc:
+				if exc.error_code == 403:
+					print("The user who posted {} is no longer publicly visible.".format(id))
+					user = "n/a"
+				else:
+					raise
+			if not user in users:
+				users[user] = 1
+			else:
+				users[user] += 1
+			print("Saved {} by {}.".format(id, user))
+		saved["users"] = users # don't do this partially
+
+	dorfleaks = twitter.search(q='#dorfleaks', count=1024, result_type="recent")["statuses"]
+
+	for status in dorfleaks:
+		#print(status.keys())
+		save(saved, status)
 		#print(status["text"], dateutil.parser.parse(status["created_at"]))
 
 saved_file = open("dorfleaks-saved.json", "w")
