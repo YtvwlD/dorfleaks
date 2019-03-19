b6403d9bc6740a271947259d1cb22513af1e44c8
Niklas Sombert <niklas@ytvwld.de>
2019-03-19 14:05:59 +0100

    cron: React to failures.

--- cron.sh
+++ cron.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-./save.py
+./save.py || exit 1
 for WHAT in per_date per_weekday per_weekday_week per_user
 do
-        ./graph.py --what $WHAT --fetch_offline --to_file dorfleaks_$WHAT.html
+        ./graph.py --what $WHAT --fetch_offline --to_file dorfleaks_$WHAT.html || exit 1
 done
+exit 0
