3435b3281ddac0a32fd53091202dc7edc02b5354
Niklas Sombert <niklas@ytvwld.de>
2021-01-20 23:44:05 +0100

    Get rid of some linter warnings

--- graph.py
+++ graph.py
@@ -4,15 +4,38 @@ import argparse
 import sys
 import datetime
 
-parser = argparse.ArgumentParser(description="Plots various statistics about #dorfleaks")
-parser.add_argument("--what", type=str, choices=["per_date", "per_weekday", "per_weekday_week", "per_user"], default='per_date')
-parser.add_argument("--from_url", type=str, help="path to dorfleaks-saved.json", default="https://pub.ytvwld.de/dorfleaks.json", metavar="URL")
-parser.add_argument("--from_file", type=str, help="path to dorfleaks-saved.json", default="dorfleaks-saved.json", metavar="FILE")
-parser.add_argument("--to_file", type=str, help="output filename", default="", metavar="FILE")
-parser.add_argument("--fetch_offline", help="load the data from the specified file", action="store_true")
-parser.add_argument("--plot_online", help="plot the data using plot.ly", action="store_true")
-parser.add_argument("--template", help="select the design to use", default="plotly")
-parser.add_argument("--display", help="display the graph after plotting", action="store_true")
+parser = argparse.ArgumentParser(
+    description="Plots various statistics about #dorfleaks"
+)
+parser.add_argument(
+    "--what", type=str,
+    choices=["per_date", "per_weekday", "per_weekday_week", "per_user"],
+    default='per_date',
+)
+parser.add_argument(
+    "--from_url", type=str, help="path to dorfleaks-saved.json",
+    default="https://pub.ytvwld.de/dorfleaks.json", metavar="URL",
+)
+parser.add_argument(
+    "--from_file", type=str, help="path to dorfleaks-saved.json",
+    default="dorfleaks-saved.json", metavar="FILE",
+)
+parser.add_argument(
+    "--to_file", type=str, help="output filename", default="", metavar="FILE",
+)
+parser.add_argument(
+    "--fetch_offline", help="load the data from the specified file",
+    action="store_true",
+)
+parser.add_argument(
+    "--plot_online", help="plot the data using plot.ly", action="store_true"
+)
+parser.add_argument(
+    "--template", help="select the design to use", default="plotly"
+)
+parser.add_argument(
+    "--display", help="display the graph after plotting", action="store_true"
+)
 args = parser.parse_args()
 
 print("Fetching...", file=sys.stderr)
@@ -29,8 +52,12 @@ else:
 
 print("Plotting...", file=sys.stderr)
 
-def _date_from_iso(date_iso):
-    return datetime.date(*map(int, date_iso.split("-"))) # Why is there no fromiso?
+
+def _date_from_iso(date_iso: str) -> datetime.date:
+    return datetime.date(
+        *map(int, date_iso.split("-"))
+    )  # fromiso only exists since 3.7
+
 
 _WEEKDAYS = ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
 
@@ -39,8 +66,8 @@ count = dict()
 
 if args.what == "per_date":
     data += [plotly.graph_objs.Bar(
-        x = list(saved["count"].keys()),
-        y = list(saved["count"].values())
+        x=list(saved["count"].keys()),
+        y=list(saved["count"].values()),
     )]
     layout = plotly.graph_objs.Layout(
         title="tweets containing #dorfleaks per date",
@@ -53,8 +80,8 @@ elif args.what == "per_weekday":
         date = _date_from_iso(date_iso)
         count[date.weekday()] += saved["count"][date_iso]
     data += [plotly.graph_objs.Bar(
-        x = _WEEKDAYS,
-        y = list(count.values())
+        x=_WEEKDAYS,
+        y=list(count.values()),
     )]
     layout = plotly.graph_objs.Layout(
         title="tweets containing #dorfleaks per weekday",
@@ -64,16 +91,16 @@ elif args.what == "per_weekday_week":
     for date_iso in saved["count"].keys():
         date = _date_from_iso(date_iso)
         week = date.isocalendar()[1]
-        if not week in count.keys():
+        if week not in count.keys():
             count[week] = {}
             for day in range(0, 7):
                 count[week][day] = 0
         count[week][date.weekday()] += saved["count"][date_iso]
     for week in count.keys():
         data += [plotly.graph_objs.Bar(
-            x = _WEEKDAYS,
-            y = list(count[week].values()),
-            name = "week #{}".format(week)
+            x=_WEEKDAYS,
+            y=list(count[week].values()),
+            name="week #{}".format(week),
         )]
     layout = plotly.graph_objs.Layout(
         title="tweets containing #dorfleaks per weekday in each week",
@@ -82,8 +109,8 @@ elif args.what == "per_weekday_week":
     )
 elif args.what == "per_user":
     data += [plotly.graph_objs.Bar(
-        x = list(saved["users"].keys()),
-        y = list(saved["users"].values())
+        x=list(saved["users"].keys()),
+        y=list(saved["users"].values())
     )]
     layout = plotly.graph_objs.Layout(
         title="tweets containing #dorfleaks per user",
@@ -100,7 +127,9 @@ else:
 
 figure = plotly.graph_objs.Figure(data=data, layout=layout)
 
-filename = args.to_file if args.to_file else "dorfleaks_{}.html".format(args.what)
+filename = (
+    args.to_file if args.to_file else "dorfleaks_{}.html".format(args.what)
+)
 
 if args.plot_online:
     # You will need to login to plot.ly beforehand.
@@ -113,7 +142,9 @@ else:
         print(plotly.offline.plot(figure, output_type="div"))
         url = "-"
     else:
-        url = plotly.offline.plot(figure, output_type="file", filename=filename, auto_open=False)
+        url = plotly.offline.plot(
+            figure, output_type="file", filename=filename, auto_open=False
+        )
 print("Saved to {}.".format(url), file=sys.stderr)
 if args.display:
     if args.to_file == "-":
--- save.py
+++ save.py
@@ -8,74 +8,82 @@ import json
 from os import environ
 from sys import argv
 from collections import Counter
+import typing
 
 if APP_KEY or APP_SECRET:
-	print("[WARN]: Please set the api secrets via environment.")
+    print("[WARN]: Please set the api secrets via environment.")
 
 APP_KEY = environ["TWITTER_APP_KEY"] if "TWITTER_APP_KEY" in environ else APP_KEY
 APP_SECRET = environ["TWITTER_APP_SECRET"] if "TWITTER_APP_SECRET" in environ else APP_SECRET
 
 saved = None
 try:
-	saved_file = open("dorfleaks-saved.json", "r")
-	saved = json.load(saved_file)
-	saved["users"] = Counter(saved["users"])
-	saved["count"] = Counter(saved["count"])
-	saved_file.close()
+    saved_file = open("dorfleaks-saved.json", "r")
+    saved = json.load(saved_file)
+    saved["users"] = Counter(saved["users"])
+    saved["count"] = Counter(saved["count"])
+    saved_file.close()
 except FileNotFoundError:
-	pass
+    pass
 
 if not saved:
-	saved = dict()
-	saved["count"] = dict()
-	saved["ids"] = list()
+    saved = dict()
+    saved["count"] = dict()
+    saved["ids"] = list()
 
 twitter = twython.Twython(APP_KEY, APP_SECRET)
 
+
 def save(saved, status):
-	if not status["retweeted"] and not status["text"].startswith("RT @"):
-		date = str(dateutil.parser.parse(status["created_at"]).date())
-		id = status["id"]
-		user = status["user"]["screen_name"]
-		if id not in saved["ids"]:
-			saved["count"][date] += 1
-			saved["users"][user] += 1
-			print("Saved {} on {} by {}.".format(id, date, user))
-			saved["ids"].append(id)
+    if not status["retweeted"] and not status["text"].startswith("RT @"):
+        date = str(dateutil.parser.parse(status["created_at"]).date())
+        id = status["id"]
+        user = status["user"]["screen_name"]
+        if id not in saved["ids"]:
+            saved["count"][date] += 1
+            saved["users"][user] += 1
+            print("Saved {} on {} by {}.".format(id, date, user))
+            saved["ids"].append(id)
+
 
 if len(argv) == 2:
-	try:
-		id = int(argv[-1])
-	except ValueError:
-		id = int(argv[-1].split("/")[-1])
-	print("Saving the specified tweet: {}".format(id))
-	status = twitter.show_status(id=id)
-	save(saved, status)
+    try:
+        id = int(argv[-1])
+    except ValueError:
+        id = int(argv[-1].split("/")[-1])
+    print("Saving the specified tweet: {}".format(id))
+    status = twitter.show_status(id=id)
+    save(saved, status)
 
 else:
-	if not "users" in saved:
-		print("Gathering additional data about existing tweets...")
-		users = Counter()
-		for id in saved["ids"]:
-			try:
-				status = twitter.show_status(id=id)
-				user = status["user"]["screen_name"]
-			except twython.exceptions.TwythonError as exc:
-				if exc.error_code == 403:
-					print("The user who posted {} is no longer publicly visible.".format(id))
-					user = "n/a"
-				else:
-					raise
-			users[user] += 1
-			print("Saved {} by {}.".format(id, user))
-		saved["users"] = users # don't do this partially
+    if "users" not in saved:
+        print("Gathering additional data about existing tweets...")
+        users = Counter()  # type: typing.Counter[str]
+        for id in saved["ids"]:
+            try:
+                status = twitter.show_status(id=id)
+                user = status["user"]["screen_name"]
+            except twython.exceptions.TwythonError as exc:
+                if exc.error_code == 403:
+                    print(
+                        "The user who posted {} is no longer publicly visible."
+                        .format(id)
+                    )
+                    user = "n/a"
+                else:
+                    raise
+            users[user] += 1
+            print("Saved {} by {}.".format(id, user))
+        saved["users"] = users  # don't do this partially
 
-	dorfleaks = twitter.search(q='#dorfleaks', count=1024, result_type="recent")["statuses"]
+    dorfleaks = twitter.search(
+        q='#dorfleaks', count=1024, result_type="recent"
+    )["statuses"]
 
-	for status in dorfleaks:
-		#print(status.keys())
-		save(saved, status)
-		#print(status["text"], dateutil.parser.parse(status["created_at"]))
+    for status in dorfleaks:
+        # print(status.keys())
+        save(saved, status)
+        # print(status["text"], dateutil.parser.parse(status["created_at"]))
 
 saved["users"] = dict(saved["users"])
 saved["count"] = dict(saved["count"])
