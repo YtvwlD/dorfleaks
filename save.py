0377c05a998a2e56019e34c1e1876750b4f9060a
Niklas Sombert <niklas@ytvwld.de>
2024-01-25 20:17:23 +0100

    Make save usable from graph

--- save.py
+++ save.py
@@ -37,29 +37,14 @@ class Toot:
         return d
 
 
-# deserialize
-saved = None
-try:
-    with open("dorfleaks-saved.json", "r") as saved_file:
-        saved = json.load(saved_file)
-        saved["toots"] = {
-            id: Toot.from_json(toot)
-            for (id, toot) in saved.get("toots", {}).items()
-        }
-except FileNotFoundError:
-    pass
-
-if not saved:
-    saved = dict()
-
-# fetching individual statuses requires authentication
-mastodon = Mastodon(
-    api_base_url="https://chaos.social/",
-    client_id=environ.get("MASTODON_CLIENT_ID"),
-    client_secret=environ.get("MASTODON_CLIENT_SECRET"),
-    access_token=environ.get("MASTODON_ACCESS_TOKEN"),
-)
-mastodon.app_verify_credentials()
+def deserialize(input):
+    """Deserialize the saved data."""
+    result = input.copy()
+    result["toots"] = {
+        id: Toot.from_json(toot)
+        for (id, toot) in input.get("toots", {}).items()
+    }
+    return result
 
 def save(saved, toot):
     if toot.id not in saved["toots"].keys():
@@ -67,27 +52,43 @@ def save(saved, toot):
             saved["toots"][toot.id] = toot
 
 
-if len(argv) == 2:
-    id = argv[-1]
-    results = mastodon.search_v2(id, resolve=True, result_type="statuses", exclude_unreviewed=False)
-    result, = results["statuses"]
-    save(saved, Toot.from_api(result))
+if __name__ == "__main__":
+    try:
+        with open("dorfleaks-saved.json", "r") as saved_file:
+            saved = json.load(saved_file)
+    except FileNotFoundError:
+        saved = dict()
+    saved = deserialize(saved)
+    # fetching individual statuses requires authentication
+    mastodon = Mastodon(
+        api_base_url="https://chaos.social/",
+        client_id=environ.get("MASTODON_CLIENT_ID"),
+        client_secret=environ.get("MASTODON_CLIENT_SECRET"),
+        access_token=environ.get("MASTODON_ACCESS_TOKEN"),
+    )
+    mastodon.app_verify_credentials()
 
-else:
-    # move all twitter data
-    if "twitter" not in saved.keys():
-        saved = {"twitter": saved}
-    
-    # the actual logic:
-    # get all toots with this hashtag
-    # remove all non-relevant data
-    toots = map(Toot.from_api, mastodon.timeline_hashtag("dorfleaks"))
-    # merge with existing data
-    saved["toots"] = saved.get("toots", {})
-    for toot in toots:
-        save(saved, toot)
+    if len(argv) == 2:
+        id = argv[-1]
+        results = mastodon.search_v2(id, resolve=True, result_type="statuses", exclude_unreviewed=False)
+        result, = results["statuses"]
+        save(saved, Toot.from_api(result))
 
-# serialize
-saved["toots"] = {id: toot.to_json() for (id, toot) in saved["toots"].items()}
-with open("dorfleaks-saved.json", "w") as saved_file:
-    json.dump(saved, saved_file)
+    else:
+        # move all twitter data
+        if "twitter" not in saved.keys():
+            saved = {"twitter": saved}
+        
+        # the actual logic:
+        # get all toots with this hashtag
+        # remove all non-relevant data
+        toots = map(Toot.from_api, mastodon.timeline_hashtag("dorfleaks"))
+        # merge with existing data
+        saved["toots"] = saved.get("toots", {})
+        for toot in toots:
+            save(saved, toot)
+
+    # serialize
+    saved["toots"] = {id: toot.to_json() for (id, toot) in saved["toots"].items()}
+    with open("dorfleaks-saved.json", "w") as saved_file:
+        json.dump(saved, saved_file)
